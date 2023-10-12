13c07664fd43de035a55c161842f85cc80122092
Adam <N00211418@iadt.ie>
2023-10-12 01:11:06 +0100

    added local storage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,36 +1,80 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Todo from './Todo'
 import { Button, Card, ListGroup } from 'react-bootstrap'
 
 const TodoList = () => {
 
 
+	
+
 	let initialList = [
 		{
-			id:0,
+			id: 0,
 			text: 'this is the todo text',
+			done: false
 		},
 		{
-			id:1,
+			id: 1,
 			text: 'this is the other todo text',
+			done: true
 		},
 	]
-
-	const [list, setList] = useState(initialList)
+	
 	const [textInput, setTextInput] = useState('')
 
-	const addTodoItem = () => {
-		let newTodo = {
-			id: list[list.length - 1],
-			text: textInput,
-		}
+	// if the list is in localstorage and its element is null
+	// local storage has not started assign initial list
+	// other wise use list in local storage
+	const [list, setList] = useState(
+		typeof(JSON.parse(window.localStorage.getItem('list'))[0]) === null ?
+		initialList :
+		JSON.parse(window.localStorage.getItem('list'))
+	)
 
-		setList([...list, newTodo ])
+	// when list changes update local storage
+	useEffect(() => {
+		window.localStorage.setItem('list', JSON.stringify(list))
+	}, [list])
+
+	const markDone = (id) => {
+		const newList = list.map((item) => {
+			if (item.id === id) {
+				item.done = true
+			}
+
+			return item
+		})
+
+		setList(newList)
 	}
 
 	const removeTodo = (id) => {
-		console.log('delete id ' + id)
+		const newList = list.filter((item) => item.id != id)
+		setList(newList)
 	}
+
+	const addTodoItem = () => {
+
+		let newTodo
+
+		if (list.length === 0) {
+			newTodo = {
+				id: 0,
+				text: textInput,
+			}
+
+		}
+		else {
+			newTodo = {
+				id: list[list.length - 1].id + 1,
+				text: textInput,
+			}
+
+		}
+
+		setList([...list, newTodo])
+	}
+
 
 	const handleTextInput = (e) => {
 		setTextInput(e.target.value)
@@ -42,7 +86,7 @@ const TodoList = () => {
 	}
 
 	const todoArray = list.map((element) => {
-		return <Todo key={element.id} todo={element} remove={removeTodo} />
+		return <Todo key={element.id} todo={element} markDone={markDone} remove={removeTodo} />
 	})
 
 	return (
